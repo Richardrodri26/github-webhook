ec59d27d47b0d7d811287600515f4618f4981196
RichardRodri26 <richardmanuel811@gmail.com>
2025-09-17 22:07:43 -0500

    feat: add Discord webhook integration and GitHub event handling

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,22 +1,50 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { GitHubService } from '../services/github.service';
+import { DiscordService } from '../services/discord.service';
+
 
 
 export class GithubController {
+
+
   constructor(
+    private readonly githubService = new GitHubService(),
+    private readonly discordService = new DiscordService(),
+  ){}
 
-  ) {}
 
-  webhookHandler = (req: Request, res: Response) => {
-    const githubEvent = req.header("x-github-event") ?? "unknown";
-    const signature = req.header("x-hub-signature-256") ?? "no-signature";
+  webhookHandler = async ( req: Request, res: Response ) => {
+
+    const githubEvent = req.header('x-github-event') ?? 'unknown';
     const payload = req.body;
+    let message:string;
 
-    console.log(`Received ${githubEvent} event:`, payload);
-    console.log('signature :>> ', signature);
+    switch( githubEvent ) {
 
-    res.status(201).send("Accepted")
+      case 'star':
+        message = this.githubService.onStar( payload );
+      break;
+
+      case 'issues':
+        message = this.githubService.onIssue(payload);
+      break;
+
+
+      default:
+        message = `Unknown event ${ githubEvent }`;
+
+    }
+    
+    try {
+      await this.discordService.notify(message);
+      res.status(202).send('Accepted');
+    } catch (error) {
+      res.status(500).json({ error: 'internal server error'});
+    }
+
   }
 
 
 
 }
+
